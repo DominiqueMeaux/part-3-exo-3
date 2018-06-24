12dba03d98e5b4c610c90326329c9f772eb4ecbe
dominiquem <dominique.meaux@gmail.com>
2018-06-24 22:46:48 +0200

    changement couleur texte

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//fonction permettant de changer la couleur du texte en cliquant sur un bouton
+function green(){
+  document.getElementById('text').style.color = 'green';
+}
+function red(){
+  document.getElementById('text').style.color = 'red';
+}
+function blue(){
+  document.getElementById('text').style.color = 'blue';
+}
